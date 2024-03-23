f10c684d2d8115850cf36dfe38b6e8bb74d0325a
Akshit Dudeja <dudejaakshit@gmail.com>
2024-03-23 14:13:54 +0530

    basic setuo

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
+// import reducers from "./redux/reducers";
+import thunk from "redux-thunk";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* <Provider> */}
+    <Router>
+      <App />
+    </Router>
+    {/* </Provider> */}
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
