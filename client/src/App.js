eebbe130eb17d85911464abf8865d58d887a18c4
Akshit Dudeja <dudejaakshit@gmail.com>
2024-03-24 19:58:45 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,31 +3,36 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Components/login/Login";
 
 import StudentLogin from "./Components/login/studentLogin/StudentLogin";
-import StudentHome from "./Components/Student/StudentHome";
-import Profile from "./Components/Student/profile/Profile";
-import StudentProfileUpdate from "./Components/Student/profile/Update/Update";
-import TestResult from "./Components/Student/TestResults/TestResult";
-import Attendance from "./Components/Student/attendance/Attendance";
-import Feedback from "./Components/Student/Feedback/Feedback";
-import DataTable1 from "./Pages/registerCourses";
-import Registration from "./Pages/Registration";
-import SeeCourses from "./Pages/SeeCourses";
+import StudentHome from "./Components/student/StudentHome";
+import Profile from "./Components/student/profile/Profile";
+// import StudentProfileUpdate from "./Components/student/profile/Update/Update";
+import TestResult from "./Components/student/TestResults/TestResult";
+
+import Attendance from "./Components/student/attendance/DisplayAttendance";
+import Feedback from "./Components/student/Feedback/Feedback";
+
+// import Registration from "./Pages/Registration";
+// import SeeCourses from "./pages/SeeCourses";
+import AdminLogin from "./Components/login/adminLogin/AdminLogin";
 
 const App = () => {
   return (
     <Routes>
-
-      <Route exact path="/" element={<Login/>} />
+      <Route exact path="/" element={<Login />} />
       <Route path="/login/studentlogin" element={<StudentLogin />} />
       <Route path="/student/home" element={<StudentHome />} />
-      <Route path="/student/home/profile" element={<Profile/>}/>
-      <Route path="/student/home/profile/update" element={<StudentProfileUpdate/>}/>
-      <Route path="/student/testResult" element={<TestResult/>}/>
-      <Route path="/student/attendance" element={<Attendance/>}/>
-      <Route path="/student/feedback" element={<Feedback/>}/>
-      <Route path="/student/courseRegistration" element={<Registration/>}/>
-      <Route path="/student/getCourses" element={<SeeCourses/>}/>
-      
+      <Route path="/student/home/profile" element={<Profile />} />
+      {/* <Route
+        path="/student/home/profile/update"
+        element={<StudentProfileUpdate />}
+      /> */}
+      <Route path="/student/testResult" element={<TestResult />} />
+      <Route path="/student/attendance" element={<Attendance />} />
+      <Route path="/student/feedback" element={<Feedback />} />
+      {/* <Route path="/student/courseRegistration" element={<Registration />} />
+      <Route path="/student/getCourses" element={<SeeCourses />} /> */}
+      <Route path="/admin/login" element={<AdminLogin />} />
+      {/* <Route path="/student/home" element={<StudentHome />} /> */}
     </Routes>
   );
 };
