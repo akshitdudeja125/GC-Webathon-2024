d9562f5fe2a976ec4f9a100a6914a7748fbe1c7d
VARSHA SWARAJ <126698394+callmeVAMP@users.noreply.github.com>
2024-03-25 08:40:28 +0530

    Update body.js
    updated body

--- client/src/Components/admin/addStudent/body.js
+++ client/src/Components/admin/addStudent/body.js
@@ -21,19 +21,11 @@ const Body = () => {
 
   const [value, setValue] = useState({
     name: "",
-    dob: "",
     email: "",
-    department: "",
-    contactNumber: "",
-    avatar: "",
+    rollNumber:"",
+    school:"",
     batch: "",
-    gender: "",
-    year: "",
-    fatherName: "",
-    motherName: "",
-    section: "",
-    fatherContactNumber: "",
-    motherContactNumber: "",
+    adminEmail:""
   });
 
   useEffect(() => {
@@ -56,19 +48,11 @@ const Body = () => {
       dispatch({ type: ADD_STUDENT, payload: true });
       setValue({
         name: "",
-        dob: "",
         email: "",
-        department: "",
-        contactNumber: "",
-        avatar: "",
+        rollNumber:"",
+        school:"",
         batch: "",
-        gender: "",
-        year: "",
-        fatherName: "",
-        motherName: "",
-        section: "",
-        fatherContactNumber: "",
-        motherContactNumber: "",
+        adminEmail:""
       });
     } catch (error) {
       if (error.response) {
@@ -86,245 +70,133 @@ const Body = () => {
 
   return (
     <div>
-    <div className="flex-[0.8] mt-3">
-      <div className="space-y-5">
-        <div className="flex text-gray-400 items-center space-x-2">
-          <AddIcon />
-          <h1>Add Student</h1>
-        </div>
-        <div className=" mr-10 bg-white flex flex-col rounded-xl ">
-          <form
-            className={'${classes.adminForm0} scrollbar-thin scrollbar-track-white scrollbar-thumb-black overflow-y-scroll h-[30rem]'}
-            onSubmit={handleSubmit}>
-            <div className={classes.adminForm1}>
-              <div className={classes.adminForm2l}>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Name :</h1>
-                  <input
-                    placeholder="Full Name"
-                    required
-                    className={classes.adminInput}
-                    type="text"
-                    value={value.name}
-                    onChange={(e) => setValue({ ...value, name: e.target.value })}
-                  />
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>DOB :</h1>
-                  <input
-                    required
-                    placeholder="DD/MM/YYYY"
-                    className={classes.adminInput}
-                    type="date"
-                    value={value.dob}
-                    onChange={(e) => setValue({ ...value, dob: e.target.value })}
-                  />
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Email :</h1>
-                  <input
-                    placeholder="Email"
-                    required
-                    className={classes.adminInput}
-                    type="email"
-                    value={value.email}
-                    onChange={(e) => setValue({ ...value, email: e.target.value })}
-                  />
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Batch :</h1>
-                  <input
-                    required
-                    placeholder="yyyy-yyyy"
-                    className={classes.adminInput}
-                    type="text"
-                    value={value.batch}
-                    onChange={(e) => setValue({ ...value, batch: e.target.value })}
-                  />
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Father's Name :</h1>
-                  <input
-                    required
-                    placeholder="Father's Name"
-                    className={classes.adminInput}
-                    type="text"
-                    value={value.fatherName}
-                    onChange={(e) => setValue({ ...value, fatherName: e.target.value })}
-                  />
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Mother's Name :</h1>
-                  <input
-                    required
-                    placeholder="Mother's Name"
-                    className={classes.adminInput}
-                    type="text"
-                    value={value.motherName}
-                    onChange={(e) => setValue({ ...value, motherName: e.target.value })}
-                  />
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Year :</h1>
-                  <Select
-                    required
-                    displayEmpty
-                    sx={{ height: 36 }}
-                    inputProps={{ "aria-label": "Without label" }}
-                    value={value.year}
-                    onChange={(e) => setValue({ ...value, year: e.target.value })}
-                  >
-                    <MenuItem value="">None</MenuItem>
-                    <MenuItem value="1">1</MenuItem>
-                    <MenuItem value="2">2</MenuItem>
-                    <MenuItem value="3">3</MenuItem>
-                    <MenuItem value="4">4</MenuItem>
-                  </Select>
-                </div>
-              </div>
-              <div className={classes.adminForm2r}>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Department :</h1>
-                  <Select
-                    required
-                    displayEmpty
-                    sx={{ height: 36 }}
-                    inputProps={{ "aria-label": "Without label" }}
-                    value={value.department}
-                    onChange={(e) => setValue({ ...value, department: e.target.value })}
-                  >
-                    <MenuItem value="">None</MenuItem>
-                    {departments?.map((dp, idx) => (
-                      <MenuItem key={idx} value={dp.department}>
-                        {dp.department}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Gender :</h1>
-                  <Select
-                    required
-                    displayEmpty
-                    sx={{ height: 36 }}
-                    inputProps={{ "aria-label": "Without label"
-                    }}
-                    value={value.gender}
-                    onChange={(e) => setValue({ ...value, gender: e.target.value })}
-                  >
-                    <MenuItem value="">None</MenuItem>
-                    <MenuItem value="Male">Male</MenuItem>
-                    <MenuItem value="Female">Female</MenuItem>
-                    <MenuItem value="Other">Other</MenuItem>
-                  </Select>
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Contact Number :</h1>
-                  <input
-                    required
-                    placeholder="Contact Number"
-                    className={classes.adminInput}
-                    type="number"
-                    value={value.contactNumber}
-                    onChange={(e) => setValue({ ...value, contactNumber: e.target.value })}
-                  />
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>
-                    Father's Contact Number :
-                  </h1>
-                  <input
-                    required
-                    placeholder="Father's Contact Number"
-                    className={classes.adminInput}
-                    type="number"
-                    value={value.fatherContactNumber}
-                    onChange={(e) => setValue({ ...value, fatherContactNumber: e.target.value })}
-                  />
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>
-                    Mother's Contact Number :
-                  </h1>
-                  <input
-                    required
-                    placeholder="Mother's Contact Number"
-                    className={classes.adminInput}
-                    type="number"
-                    value={value.motherContactNumber}
-                    onChange={(e) => setValue({ ...value, motherContactNumber: e.target.value })}
-                  />
-                </div>
-                <div className={classes.adminForm3}>
-                  <h1 className={classes.adminLabel}>Avatar :</h1>
-                  <FileBase
-                    type="file"
-                    multiple={false}
-                    onDone={({ base64 }) => setValue({ ...value, avatar: base64 })}
-                  />
+      <div className="flex-[0.8] mt-3">
+        <div className="space-y-5">
+          <div className="flex text-gray-400 items-center space-x-2">
+            <AddIcon />
+            <h1>Add Student</h1>
+          </div>
+          <div className="mr-10 bg-white flex flex-col rounded-xl">
+            <form
+              className={`${classes.adminForm0} scrollbar-thin scrollbar-track-white scrollbar-thumb-black overflow-y-scroll h-[30rem]`}
+              onSubmit={handleSubmit}
+            >
+              <div className={classes.adminForm1}>
+                <div className={classes.adminForm2l}>
+                  <div className={classes.adminForm3}>
+                    <h1 className={classes.adminLabel}>Name :</h1>
+                    <input
+                      placeholder="Full Name"
+                      required
+                      className={classes.adminInput}
+                      type="text"
+                      value={value.name}
+                      onChange={(e) => setValue({ ...value, name: e.target.value })}
+                    />
+                  </div>
+                  
+                  <div className={classes.adminForm3}>
+                    <h1 className={classes.adminLabel}>Email :</h1>
+                    <input
+                      placeholder="Email"
+                      required
+                      className={classes.adminInput}
+                      type="email"
+                      value={value.email}
+                      onChange={(e) => setValue({ ...value, email: e.target.value })}
+                    />
+                  </div>
+                  <div className={classes.adminForm3}>
+                    <h1 className={classes.adminLabel}>Roll Number :</h1>
+                    <input
+                      required
+                      placeholder=""
+                      className={classes.adminInput}
+                      type="text"
+                      value={value.rollNumber}
+                      onChange={(e) => setValue({ ...value, rollNumber: e.target.value })}
+                    />
+                  </div>
+                  <div className={classes.adminForm3}>
+                    <h1 className={classes.adminLabel}>Batch :</h1>
+                    <input
+                      required
+                      placeholder="yyyy-yyyy"
+                      className={classes.adminInput}
+                      type="text"
+                      value={value.batch}
+                      onChange={(e) => setValue({ ...value, batch: e.target.value })}
+                    />
+                  </div>
+                  <div className={classes.adminForm3}>
+                    <h1 className={classes.adminLabel}>School :</h1>
+                    <input
+                      required
+                      placeholder=""
+                      className={classes.adminInput}
+                      type="text"
+                      value={value.school}
+                      onChange={(e) => setValue({ ...value, school: e.target.value })}
+                    />
+                  </div>
+                  <div className={classes.adminForm3}>
+                    <h1 className={classes.adminLabel}>Admin Email :</h1>
+                    <input
+                      placeholder="Admin Email"
+                      required
+                      className={classes.adminInput}
+                      type="email"
+                      value={value.adminEmail}
+                      onChange={(e) => setValue({ ...value, adminEmail: e.target.value })}
+                    />
+                  </div>
+                  <div className={classes.adminFormButton}>
+                    <button className={classes.adminFormSubmitButton} type="submit">
+                      Submit
+                    </button>
+                    <button
+                      onClick={() => {
+                        setValue({
+                          name: "",
+                          email: "",
+                          rollNumber:"",
+                          school:"",
+                          batch: "",
+                          adminEmail:""
+                        });
+                        setError({});
+                      }}
+                      className={classes.adminFormClearButton}
+                      type="button"
+                    >
+                      Clear
+                    </button>
+                  </div>
+                  <div ref={errorRef} className={classes.loadingAndError}>
+                    {loading && (
+                      <Spinner
+                        message="Adding Student"
+                        height={30}
+                        width={150}
+                        color="#111111"
+                        messageColor="blue"
+                      />
+                    )}
+                    {(error.emailError || error.backendError) && (
+                      <p className="text-red-500">
+                        {error.emailError || error.backendError}
+                      </p>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className={classes.adminFormButton}>
-              <button className={classes.adminFormSubmitButton} type="submit">
-                Submit
-              </button>
-              <button
-                onClick={() => {
-                  setValue({
-                    name: "",
-                    dob: "",
-                    email: "",
-                    department: "",
-                    contactNumber: "",
-                    avatar: "",
-                    batch: "",
-                    gender: "",
-                    year: "",
-                    fatherName: "",
-                    motherName: "",
-                    section: "",
-                    fatherContactNumber: "",
-                    motherContactNumber: "",
-                  });
-                  setError({});
-                }}
-                className={classes.adminFormClearButton}
-                type="button"
-              >
-                Clear
-              </button>
-            </div>
-            <div ref={errorRef} className={classes.loadingAndError}>
-              {loading && (
-                <Spinner
-                  message="Adding Student"
-                  height={30}
-                  width={150}
-                  color="#111111"
-                  messageColor="blue"
-                />
-              )}
-              {(error.emailError || error.backendError) && (
-                <p className="text-red-500">
-                  {error.emailError || error.backendError}
-                </p>
-              )}
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
+        <ToastContainer />
       </div>
-      <ToastContainer />
-    </div>
     </div>
   );
 };
 
-export default Body;
-
-
-
-
-
-
-
+export default Body;
