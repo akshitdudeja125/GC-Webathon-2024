ba52a1d657646106549768d26f6e47280895ed6f
Akshit Dudeja <dudejaakshit@gmail.com>
2024-03-25 21:53:10 +0530

    final

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,10 @@ import studentRoutes from "./router/student";
 import adminRoutes from "./router/admin";
 import facultyRoutes from "./router/faculty";
 import eventRoutes from "./router/event";
+import https from "https";
+const cron = require("node-cron");
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -20,6 +23,24 @@ app.get("/", (req, res) => {
   res.send("Hello to college erp API");
 });
 
+
+const backendUrl = "https://gc-webathon-2024.onrender.com";
+cron.schedule("*/180 * * * * *", async function () {
+  console.log("Restarting server");
+
+  await https
+    .get(backendUrl, (res) => {
+      if (res.statusCode === 200) {
+        console.log("Restarted");
+      } else {
+        console.error(`failed to restart with status code: ${res.statusCode}`);
+      }
+    })
+    .on("error", (err) => {
+      console.log("hi");
+      console.error("Error ", err.message);
+    });
+});
 
 export default app;
 
